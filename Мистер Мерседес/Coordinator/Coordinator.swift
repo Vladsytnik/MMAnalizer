ecc88df77cfbeda5e479c512ff0aeb37315a9a07
Vlad Sytnik <64352783+Vladsytnik@users.noreply.github.com>
2022-07-16 11:22:12 +0300

    reorganize appDelegate and Coordinator

--- Мистер Мерседес/Coordinator/Coordinator.swift
+++ Мистер Мерседес/Coordinator/Coordinator.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class Coordinator {
 
-    let mainFlow: MainFlow
+    var mainFlow: MainFlow?
     let tabBarController: UITabBarController
+    
+    let carsNavController = UINavigationController()
+    let monthlyAnalisisNavController = UINavigationController()
 
     init(with tabBarController: UITabBarController) {
         self.tabBarController = tabBarController
+        
+        configureTabBarItems()
+        addNavControllersInTabBar()
+        
         self.mainFlow = MainFlow(tabBar: tabBarController)
+    }
+}
+
+extension Coordinator {
+    func configureTabBarItems() {
+        let carsImage = UIImage(systemName: "list.bullet")
+        let profitImage = UIImage(systemName: "dollarsign.square")
+        
+        let carsTitle = "Автомобили"
+        let totalProfitTitle = "Прибыль"
+        
+        carsNavController
+            .tabBarItem = UITabBarItem(
+                title: carsTitle,
+                image: carsImage,
+                selectedImage: carsImage
+            )
+        monthlyAnalisisNavController
+            .tabBarItem = UITabBarItem(
+                title: totalProfitTitle,
+                image: profitImage,
+                selectedImage: profitImage
+            )
+    }
+    
+    func addNavControllersInTabBar() {
+        self.tabBarController.viewControllers = [carsNavController, monthlyAnalisisNavController]
     }
 }
